10343ae072d9f9bbdf97c4636c6a4fd5c9bb8e29
akettle44 <andrewjkettle@gmail.com>
2020-04-06 19:54:12 -0700

    Working script

--- corona.R
+++ corona.R
@@ -9,18 +9,15 @@ GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", authenticate
 #read the Dataset sheet into “R”. The dataset will be called "data".
 corona <- read.csv(tf)
 
+#select certain columns from orignal dataframe
 scorona <- select(corona, dateRep, day, month, year, countriesAndTerritories, cases, deaths, popData2018)
 scorona <- na.omit(scorona)
 
+#change to R-readable data format
 scorona$dateRep <- as.Date(scorona$dateRep, format = "%d/%m/%y")
-scorona <- filter(scorona, dateRep != "2019/12/31" & dateRep != "2020/12/31")
+scorona <- filter(scorona, dateRep != "2020/12/31")
 
 scorona <- arrange(scorona, countriesAndTerritories, year, month, day)
-
-head(scorona)
-#scorona <- filter(scorona, countriesAndTerritories == "China")
-#thedata <- sum(scorona$cases)
-#thedata
 
 cumdata <- data.frame(matrix(ncol = 3, nrow = 0))
 names <- c("date", "country", "cases")
@@ -37,14 +34,15 @@ calcOverall <- function(df, country)
 for(country in unique(scorona$countriesAndTerritories))
 {
     temp <- calcOverall(scorona, country)
-    print(temp)
     cumdata <- rbind(cumdata, temp)
 }
-
-head(cumdata)
 
 cplot <- ggplot(data = cumdata, aes(x = date, color = country)) +
     geom_line(aes(y = cases)) +
     scale_x_date(date_labels = "%b %Y")
 
-ggplotly(cplot)
+web <- ggplotly(cplot)
+
+htmlwidgets::saveWidget(as_widget(web), "index.html")
+
+print("Script completed... Exiting")
